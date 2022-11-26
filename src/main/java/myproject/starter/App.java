ff6ae3f6ba21f65e764f066e50e113b41365ded8
Tofique Ahmed Khan <tofiquekhan171819@gmail.com>
2022-11-26 10:50:24 +0530

    Task Schedule on a Paticular Date & Time

--- src/main/java/myproject/starter/App.java
+++ src/main/java/myproject/starter/App.java
@@ -15,7 +15,8 @@ public class App
     {
     	System.out.println("App.main() start :: App is started at ::"+new Date());
     	Timer timer = new Timer();
-    	timer.schedule(new ScheduleTask(), 2000,3000);
+//    	timer.schedule(new ScheduleTask(), 2000,3000);
+    	timer.schedule(new ScheduleTask(), new Date(122, 10, 26, 10, 50, 00));
     	
     	System.out.println("End of main() -- App is Ended at "+new Date());
     }
